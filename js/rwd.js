541a35d4261c850cd5e158ecf5ecfd75695513c1
kioh629 <ds420114@naver.com>
2023-10-20 18:11:46 +0900

    레이아웃 수정

--- js/rwd.js
+++ js/rwd.js
@@ -4,11 +4,13 @@ $(function(){
     $("#ham_btn a").on("click",function(){
         $("#modal").css("display","block");
         $("#ham_wrap").animate({right:0},300);
+        $("body").addClass('body_lock')
         return false;
     });
     $(".xi-hamburger-out, #modal").on("click",function(){
         $("#modal").css("display","none");
         $("#ham_wrap").animate({right:-80+"%"},300);
+        $("body").removeClass('body_lock')
         return false;
     });
 
@@ -23,23 +25,40 @@ $(function(){
         }
     });
 
-    // 풀메뉴 여닫기
+    // 풀메뉴 마우스 엔터
     $("#main_menu li").on("mouseenter",function(){
         const a = $(this).index();
+        const sub = $("#sub_menu ul").eq(a)
         $("#main_menu li").css("color","#414141").css("border-bottom","none")
         $(this).css("color","#005BAE").css("border-bottom","5px solid #005bae")
         $("#sub_menu").slideDown();
         $("#sub_menu ul").css("display","none");
-        $("#sub_menu ul").eq(a).css("display","block");
+        sub.css("display","block");
     });
-
     $("#full_menu").on("mouseleave",function(){
-        const a = $(this).index();
         $("#main_menu li").css("color","#414141").css("border-bottom","none");
         $("#sub_menu").slideUp();
         $("#sub_menu ul").css("display","none");
     });
+    
+    // // 풀메뉴 클릭 이벤트
+    // $("#main_menu li").on("click",function(){
+    //     const a = $(this).index();
+    //     const sub = $("#sub_menu ul").eq(a);
+    //     if (sub.is(':visible')){
+    //         $("#main_menu li").css("color","#414141").css("border-bottom","none");
+    //         $("#sub_menu").slideUp();
+    //         $("#sub_menu ul").css("display","none");
+    //     } else {
+    //         $("#main_menu li").css("color","#414141").css("border-bottom","none");
+    //         $(this).css("color","#005BAE").css("border-bottom","5px solid #005bae");
+    //         $("#sub_menu").slideDown();
+    //         $("#sub_menu ul").css("display","none");
+    //         sub.css("display","block");
+    //     }
+    // });
 
+    // 배너(slick)
     $('#slide_banner').slick({
         dots: true, // 인디케이터 활성화
         autoplay: true,
