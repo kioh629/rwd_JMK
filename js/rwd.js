b7559b986abaf23aee5e996d787b64858c00a33c
kioh629 <ds420114@naver.com>
2023-10-31 18:17:26 +0900

    레이아웃 2차완성

--- js/rwd.js
+++ js/rwd.js
@@ -41,6 +41,27 @@ $(function(){
         $("#sub_menu ul").css("display","none");
     });
     
+    // 빠른 예매 상단 범주 선택
+        // 클릭 시 흰색으로
+    $('#ticket_category li').on("click",function(){
+        $('#ticket_category li').removeClass('ticket_selected');
+        $(this).addClass('ticket_selected');
+    });
+    setInterval(function(){
+        if ($('.four_man').hasClass('ticket_selected')){
+            $('#select_NumberOfPeople>li').text('총 4명의 인원을 선택하세요')
+        } else {
+            $('#select_NumberOfPeople>li').text('승객 연령 및 좌석')
+        }
+        if ($('.tour_train').hasClass('ticket_selected')){
+            $('.select_train_top .ticket_sub').text('관광열차를 선택해주세요')
+            $('.select_train_top .ticket_btn').css('display','none')
+        } else {
+            $('.select_train_top .ticket_sub').text('열차유형')
+            $('.select_train_top .ticket_btn').css('display','block')
+        }
+    });
+
     // 배너(slick)
     $('#slide_banner').slick({
         dots: true, // 인디케이터 활성화
@@ -139,7 +160,9 @@ $(function(){
     $('input#ticket_time').attr('min',year+'-'+month+'-'+date);
     $('input#depart_day').attr('value',year+'-'+month+'-'+date);
     $('input#depart_day').attr('min',year+'-'+month+'-'+date);
+    $('input#arrival_day').attr('value',year+'-'+month+'-'+date);
     $('input#arrival_day').attr('min',year+'-'+month+'-'+date);
+
         // 편도 - 왕복 선택
     $('.select_date_top .ticket_btn a:last-child').on('click',function(){
         $('.select_date_top .ticket_btn a').removeClass('btn_selected');
@@ -183,7 +206,7 @@ $(function(){
             $(this).parent().prev().attr('value',num)
         }
     });
-        // 인원 선택 시 밸류값 변경 (단독 연령일 경우 - 연령 이름도 반영)
+        // 인원 선택 시 밸류값 변경
     setInterval(function(){
         let sum = 0;
         $('.num').each(function(){
@@ -191,21 +214,71 @@ $(function(){
         });
         if (sum-parseInt($('#adult').val()) == 0){
             $('.select_num input').attr('value','어른 '+sum+'명')
-        } else if (sum-parseInt($('#child').val()) == 0){
-            $('.select_num input').attr('value','어린이 '+sum+'명')
-        } else if (sum-parseInt($('#bady').val()) == 0){
-            $('.select_num input').attr('value','유아 '+sum+'명')
-        } else if (sum-parseInt($('#senior').val()) == 0){
+        } else if (sum-parseInt($('#senior').val()) == 0) {
             $('.select_num input').attr('value','경로 '+sum+'명')
-        } else if (sum-parseInt($('#s-disabled').val()) == 0){
-            $('.select_num input').attr('value','중증장애인 '+sum+'명')
-        } else if (sum-parseInt($('#m-disabled').val()) == 0){
-            $('.select_num input').attr('value','경증장애인 '+sum+'명')
         }
         else {
             $('.select_num input').attr('value','총 '+sum+'명');
         }
     });
+
+    // 할인 선택창
+        // 할인 선택 -> 할인 유형 및 인원으로 변경
+    setInterval(function(){
+        if($('.discount_ticket').hasClass('ticket_selected')){
+            $('#select_NumberOfPeople').css('display','none');
+            $('#select_discount').css('display','block');
+        } else {
+            $('#select_NumberOfPeople').css('display','block');
+            $('#select_discount').css('display','none');
+        }
+    });
+        // 인원 - 여닫기
+    $('#dis_num').on('click', function(){
+        if ($('.select_num_window_discount').is(':visible')){
+            $('.select_num_window_discount').stop().slideUp();
+        } else {
+            $('.select_num_window_discount').stop().slideDown();
+        }
+    })
+    $(".select_num_window_discount .close_win").on("click",function(){
+        $('.select_num_window_discount').stop().slideUp();
+    });
+        // 인원 - + 버튼 작동
+        $(".dis_add_num .xi-minus-min").on("click",function(){
+            let num = $(this).parent().next().attr('value');
+            if (num > 0) {
+                num--;
+                $(this).parent().next().attr('value',num)
+            }
+        });
+        $(".dis_add_num .xi-plus-min").on("click",function(){
+            let num = $(this).parent().prev().attr('value');
+            if (num < 9) {
+                num++;
+                $(this).parent().prev().attr('value',num)
+            }
+        });
+
+        // 할인유형 - 여닫기
+    $('#discount').on('click', function(){
+        if ($('.select_dis_window').is(':visible')){
+            $('.select_dis_window').stop().slideUp();
+        } else {
+            $('.select_dis_window').stop().slideDown();
+        }
+    })
+    $(".select_dis_window .close_win").on("click",function(){
+        $('.select_dis_window').stop().slideUp();
+    });
+        // 할인유형 - 밸류값 변경
+    $(".discount_category li").on("click",function(){
+        let disName = $(this).text();
+        $("#discount").attr('value',disName);
+        $(".select_dis_window").stop().slideUp();
+    });
+    
+
 
     // 열차유형 선택
     $("#select_train_box").on("click",function(){
@@ -218,7 +291,7 @@ $(function(){
     $(".select_train_window .close_win").on("click",function(){
         $('.select_train_window').stop().slideUp();
     });
-    $(".train_category li").on("click",function(){
+    $(".select_train_window .train_category li").on("click",function(){
         let tname = $(this).text();
         $('#select_train_box').attr('value',tname);
         $('.select_train_window').stop().slideUp();
@@ -237,6 +310,41 @@ $(function(){
         return false;
     });
 
+    // 관광열차 변경 시
+    setInterval(function(){
+        if ($('.tour_train').hasClass('ticket_selected')){
+            $('.select_train_window #train_wrap .normal_train').css('display','none');
+            $('.select_train_window #train_wrap .tour_train').css('display','block');
+        } else {
+            $('.select_train_window #train_wrap .tour_train').css('display','none');
+            $('.select_train_window #train_wrap .normal_train').css('display','block');
+        }
+    });
+    $('.tour_train li:first-child').on('click',function(){
+        $('#depart').attr('value','강릉')
+        $('#arrival').attr('value','분천')
+    });
+    $('.tour_train li:nth-child(2)').on('click',function(){
+        $('#depart').attr('value','용산')
+        $('#arrival').attr('value','군산')
+    });
+    $('.tour_train li:nth-child(3)').on('click',function(){
+        $('#depart').attr('value','분천')
+        $('#arrival').attr('value','철암')
+    });
+    $('.tour_train li:nth-child(4)').on('click',function(){
+        $('#depart').attr('value','부산')
+        $('#arrival').attr('value','보성')
+    });
+    $('.tour_train li:nth-child(5)').on('click',function(){
+        $('#depart').attr('value','도라산')
+        $('#arrival').attr('value','서울')
+    });
+    $('.tour_train li:last-child').on('click',function(){
+        $('#depart').attr('value','청량리')
+        $('#arrival').attr('value','정선')
+    });
+
     // 여행상품 - 국내/국외 버튼
     $('.int_btn').on("click",function(){
         $('.trip_btn a').removeClass('p_btn_selected');
@@ -253,4 +361,8 @@ $(function(){
         $('#trip_overseas').css('display','block');
         return false;
     });
+
+    $("#scrl_top").on("click",function(){
+        $("html, body").animate({scrollTop:0},500);
+    })
 });
