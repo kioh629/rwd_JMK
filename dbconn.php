9dc209b15d757c554c4a2b1f5001b1776ead5aa8
kioh629 <ds420114@naver.com>
2023-11-01 15:55:38 +0900

    language, family site select 변경

---
+++ dbconn.php
@@ -0,0 +1,7 @@
+<?
+ $connect=mysqli_connect( "localhost", "kioh0629", "wjdaudry11z*","kioh0629") or  
+        die( "SQL server에 연결할 수 없습니다."); 
+
+    mysqli_select_db($connect,"kioh0629");
+//localhost, username,pw,dbname
+?>
